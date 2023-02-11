c2f422260d42696c960b337d094d70903568c279
WasathTheekshana <wasath.vt@gmail.com>
2023-02-11 12:15:58 +0530

    dev: CRUD developed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+
 import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState("");
+
   const [user, setUser] = useState([]);
   const usersCollectionRef = collection(db, "user");
 
+  //Delete User form database
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "user", id);
+    await deleteDoc(userDoc);
+  }
+
+  //Update database
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "user", id);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
+  //Add data to the database
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: newAge });
+  };
+
+  //Read data form database
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
@@ -25,9 +54,45 @@ function App() {
               {" "}
               <h1>Name: {user.name}</h1>
               <h1>Age: {user.age}</h1>
+              <button
+                onClick={() => {
+                  updateUser(user.id, user.age);
+                }}
+              >
+                {" "}
+                Increase Age{" "}
+              </button>
+
+              <button
+                onClick={() => {
+                  deleteUser(user.id);
+                }}
+              >
+                Delete User
+              </button>
             </div>
           );
         })}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }}
+        />
+
+        <input
+          type="text"
+          placeholder="Age"
+          onChange={(event) => {
+            setNewAge(event.target.value);
+          }}
+        />
+
+        <button onClick={createUser}>Create User</button>
       </div>
     </div>
   );
